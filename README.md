2020ce78330f41b93ce64f7f31ebb9ef9ece62cf
kanfangtingyu <735088144@qq.com>
2020-02-01 16:27:52 +0800

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# flowerManage
+鲜花管理系统
+
+
+该项目为前后端分离项目
+
+前端在client文件下，使用element框架
+
+后台在根目录文件下，server为入口文件，api编写在routes->api下
+
+数据库使用了本地mongo数据库，配置在config下，创建集合在models下
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
   mongoURI:
     'mongodb://localhost/test',
-    //'mongodb://nodevueelement:test1234@ds223756.mlab.com:23756/node-vue-element',
   secretOrKey: 'secret'
 };
